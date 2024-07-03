72a6d008f80c76dcd9c44d939ab16526ef6ff175
Jonathan Araujo <jonathan@wicadu.com>
2024-07-03 10:51:47 -0400

    refactor formatCurrency

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,1 +1,25 @@
-export default (value: number = 0): string => isNaN(value) ? '$?' : new Intl.NumberFormat('es-CL', { style: 'currency', currency: 'CLP' }).format(value)
+function formatCurrency(
+  value: number,
+  options?: Intl.NumberFormatOptions,
+  locale?: Intl.LocalesArgument
+) {
+  try {
+    if (isNaN(value)) return '$?'
+
+    const defaultLocale: Intl.LocalesArgument = 'es-CL'
+    const defaultOptions: Intl.NumberFormatOptions = {
+      style: 'currency',
+      currency: 'CLP',
+      currencyDisplay: 'narrowSymbol',
+    }
+
+    return new Intl.NumberFormat(locale || defaultLocale, {
+      ...defaultOptions,
+      ...(options || {}),
+    }).format(value)
+  } catch (error) {
+    if (isNaN(value)) return '$?'
+  }
+}
+
+export default formatCurrency
