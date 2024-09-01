5b25430a3de5463c1f5cbc9b7f67a01aa5e9dea5
Jonathan Araujo <jonathan@wicadu.com>
2024-09-01 12:49:58 -0400

    refactor InputFile molecule

--- src/ui/molecules/InputFile/InputFile.tsx
+++ src/ui/molecules/InputFile/InputFile.tsx
@@ -1,16 +1,16 @@
-import React, { Fragment, useMemo, useState } from 'react'
+import React, { Fragment, useMemo } from 'react'
 
 import { Controller } from 'react-hook-form'
-import { SerializedStyles } from '@emotion/react'
+import { css, SerializedStyles } from '@emotion/react'
 
-import { getFormFieldsErrors } from '../../../utils'
+import { getFileSize, getFormFieldsErrors } from '../../../utils'
 import Form from '../../hocs/Form'
 import InputFileUploader from './Uploader'
-import FilesSummary from './FilesSummary'
 import InputFeedback from '../../hocs/InputFeedback'
+import Row from '../../layout/Row'
+import Button from '../../atoms/Button'
 
 interface Props {
-  component?: React.ReactNode,
   name: string
   disabled?: boolean
   defaultValue?: string | number
@@ -35,22 +35,24 @@ function InputFile({
   width,
   accept,
   disabled,
-  component,
   defaultValue,
   doNotShowFeedback,
   doNotShowErrors,
   containerStyles,
   uploaderStyles
 }) {
-  const [loadedFile, setLoadedFile] = useState<File | File[]>(null)
-
   const { control, formState: { errors } } = Form.useForm()
+  const loadedFile: File = Form.useWatch({ name, control })
 
   const fieldError = getFormFieldsErrors(errors, name)
 
   const Container = useMemo(() => doNotShowFeedback ? Fragment : InputFeedback, [
     doNotShowFeedback
   ])
+
+  const onPreviewFile = () => {
+    window.open(URL.createObjectURL(loadedFile), '_blank')
+  }
 
   return (
     <Controller
@@ -65,33 +67,41 @@ function InputFile({
           doNotShowErrors={doNotShowErrors}
           styles={containerStyles}
         >
-          {loadedFile ? (
-            <FilesSummary
-              file={loadedFile}
-              clearFile={() => {
-                onChange?.(null)
-                setLoadedFile(null)
-              }}
-            />
-          ) : (
+          <Row gap={10}>
             <InputFileUploader
               accept={accept}
               width={width}
               disabled={disabled}
-              component={component}
-              onChange={(file: File | File[]) => {
-                onChange?.(file)
-                setLoadedFile(file)
-              }}
+              onChange={(file: File | File[]) => onChange?.(file)}
               styles={uploaderStyles}
+              loadedFile={loadedFile}
             />
-          )}
+            {Boolean(loadedFile) && (
+              <Button
+                type='link'
+                align='right'
+                children={`${loadedFile?.name?.slice(-17)} - ${getFileSize(loadedFile?.size)}`}
+                onClick={onPreviewFile}
+                styles={cssPreviewButtonStyles}
+              />
+            )}
+          </Row>
         </Container>
       }
       defaultValue={defaultValue}
     />
   )
 }
+
+const cssPreviewButtonStyles = css`
+  @media screen and (min-width: 768px) {
+    width: 60% !important;
+  }
+
+  @media screen and (min-width: 1366px) {
+    width: 40%;
+  }
+`
 
 InputFile.defaultProps = defaultProps
 
