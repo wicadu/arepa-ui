f49e0d65b92dfc76a1199ee3dd32c9f74d508f59
Jonathan Araujo <jonathan@wicadu.com>
2024-12-04 03:24:54 +0000

    refactor OrderItemBottom for better SEO/Accessibility

--- src/ui/organisms/OrderItem/OrderItemBottom.tsx
+++ src/ui/organisms/OrderItem/OrderItemBottom.tsx
@@ -5,23 +5,34 @@ import Typography from '../../atoms/Typography'
 import Icon from '../../atoms/Icon'
 import Spin from '../../atoms/Spin'
 
+const translate = {
+  es: {
+    SUBTRACT_ARIA_LABEL: 'Disminuir cantidad del producto',
+    INCREASE_ARIA_LABEL: 'Aumentar cantidad del producto',
+    HANDLER_ARIA_LABEL: 'Controlador de cantidad',
+  },
+  en: {},
+  pt: {},
+}
+
 enum ActionType {
   ADD = 'ADD',
   SUBTRACT = 'SUBTRACT',
 }
 
 interface Props {
-  initialQuantity: number,
+  initialQuantity: number
   onChangeQuantity: (
     type: ActionType,
     quantity: number,
     onRevertQuantity: () => void
-  ) => void,
-  minQuantity: number,
-  maxQuantity: number,
-  disabled?: boolean,
+  ) => void
+  minQuantity: number
+  maxQuantity: number
+  disabled?: boolean
   editable?: boolean
   loading: boolean
+  lang: string
 }
 
 const defaultProps: Partial<Props> = {
@@ -29,6 +40,7 @@ const defaultProps: Partial<Props> = {
   maxQuantity: Infinity,
   disabled: false,
   loading: false,
+  lang: 'es',
 }
 
 function OrderItemBottom(props: Props) {
@@ -39,10 +51,11 @@ function OrderItemBottom(props: Props) {
     maxQuantity,
     disabled,
     editable,
-    loading
+    loading,
+    lang,
   } = {
     ...defaultProps,
-    ...props
+    ...props,
   }
 
   const [quantity, setQuantity] = useState<number>(initialQuantity)
@@ -50,40 +63,30 @@ function OrderItemBottom(props: Props) {
 
   const onRevertQuantity = useCallback(() => setRevertingQuantity(true), [])
 
-  const onChange = useCallback((type: ActionType) => {
-    let newQuantity: number = quantity
+  const onChange = useCallback(
+    (type: ActionType) => {
+      let newQuantity: number = quantity
 
-    if (type === ActionType.ADD) newQuantity += 1
-    else newQuantity -= 1
+      if (type === ActionType.ADD) newQuantity += 1
+      else newQuantity -= 1
 
-    setQuantity(newQuantity)
-    onChangeQuantity?.(type, newQuantity, onRevertQuantity)
-  }, [
-    quantity,
-    setQuantity,
-    onChangeQuantity,
-    onRevertQuantity
-  ])
+      setQuantity(newQuantity)
+      onChangeQuantity?.(type, newQuantity, onRevertQuantity)
+    },
+    [quantity, setQuantity, onChangeQuantity, onRevertQuantity]
+  )
 
   const onAddQuantity = useCallback(() => {
     if (quantity >= maxQuantity || disabled || loading) return
 
     onChange(ActionType.ADD)
-  }, [
-    onChange,
-    disabled,
-    loading
-  ])
+  }, [onChange, disabled, loading])
 
   const onSubtractQuantity = useCallback(() => {
     if (quantity <= minQuantity || disabled || loading) return
 
     onChange(ActionType.SUBTRACT)
-  }, [
-    onChange,
-    disabled,
-    loading
-  ])
+  }, [onChange, disabled, loading])
 
   useEffect(() => {
     if (initialQuantity !== quantity && revertingQuantity) {
@@ -94,25 +97,55 @@ function OrderItemBottom(props: Props) {
     if (initialQuantity !== quantity) {
       setQuantity(initialQuantity)
     }
-  }, [
-    initialQuantity,
-    revertingQuantity,
-  ])
+  }, [initialQuantity, revertingQuantity])
 
   return (
-    <Container>
-      <Icon name={editable ? 'remove' : '_'} size={15} onClick={onSubtractQuantity} />
-      {loading
-        ? <Spin center size={14} type='white' />
-        : (
-          <Typography
-            type='description'
-            size={13}
-            dangerouslySetInnerHTML={{ __html: `<b>${quantity}</b> unidades` }}
-          />
-        )
-      }
-      <Icon name={editable ? 'add' : '_'} size={15} onClick={onAddQuantity} />
+    <Container aria-label={translate[lang].HANDLER_ARIA_LABEL}>
+      <IconWrapper
+        onClick={onSubtractQuantity}
+        show={editable}
+        children={
+          <>
+            <meta
+              itemProp="name"
+              content={translate[lang].SUBTRACT_ARIA_LABEL}
+            />
+            <Icon name="remove" size={15} />
+          </>
+        }
+        aria-label={translate[lang].SUBTRACT_ARIA_LABEL}
+        itemScope
+        itemProp="action"
+        itemType="http://schema.org/Action"
+      />
+
+      {loading ? (
+        <Spin center size={14} type="white" />
+      ) : (
+        <Typography
+          type="description"
+          size={13}
+          dangerouslySetInnerHTML={{ __html: `<b>${quantity}</b> unidades` }}
+        />
+      )}
+
+      <IconWrapper
+        onClick={onAddQuantity}
+        show={editable}
+        children={
+          <>
+            <meta
+              itemProp="name"
+              content={translate[lang].INCREASE_ARIA_LABEL}
+            />
+            <Icon name="add" size={15} />
+          </>
+        }
+        aria-label={translate[lang].INCREASE_ARIA_LABEL}
+        itemProp="action"
+        itemScope
+        itemType="http://schema.org/UpdateAction"
+      />
     </Container>
   )
 }
@@ -127,7 +160,7 @@ const Container = styled.div`
   background-color: ${({ theme }) => theme.colors.NEUTRAL.SIDE};
 
   .material-icons {
-    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
     -webkit-touch-callout: none;
     -webkit-user-select: none;
     -khtml-user-select: none;
@@ -137,7 +170,7 @@ const Container = styled.div`
   }
 
   @media screen and (min-width: 768px) {
-    p[type="description"] {
+    p[type='description'] {
       font-size: 16px !important;
     }
 
@@ -145,6 +178,14 @@ const Container = styled.div`
       font-size: 20px;
     }
   }
+`
+
+const IconWrapper = styled.button`
+  background: none;
+  border: none;
+  padding: 0px;
+
+  ${({ show }) => !show && `display: none;`}
 `
 
 export default OrderItemBottom
