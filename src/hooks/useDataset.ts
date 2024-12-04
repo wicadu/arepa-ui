5dcb95dfd6774e8fcbd73ff3cce981beaa125bf4
Jonathan Araujo <jonathan@wicadu.com>
2024-12-04 03:21:28 +0000

    update useDataset to remove prefix 'data-'

--- src/hooks/useDataset.ts
+++ src/hooks/useDataset.ts
@@ -1,12 +1,12 @@
 import { useMemo } from 'react'
 
-type Dataset = Record<string, string | number | boolean>
+export type Dataset = Record<string, string | number | boolean>
 
 const useDataset = (datasets: Dataset = {}): Record<string, string> => {
   return useMemo(
     () =>
       Object.entries(datasets).reduce((prev, [key, value]) => {
-        prev[`data-${key}`] = String(value)
+        prev[key] = String(value)
         return prev
       }, {} as Record<string, string>),
     [datasets]
