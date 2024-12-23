7464f29650b7554e087a58300d3dd17356cbccda
Jonathan Araujo <jonathan@wicadu.com>
2024-12-23 04:11:51 +0000

    added try-catch in hexToRBGA to avoid breaking errors

--- src/utils/hexToRGBA.ts
+++ src/utils/hexToRGBA.ts
@@ -1,11 +1,18 @@
 export default (hex: string, alpha: number) => {
-  let content = hex.replace('#', '');
+  try {
+    let content = hex.replace('#', '')
 
-  const hexadecimal = content.length > 3 ? content.match(/[a-f\d]{2}/gi) : content.split('')
+    const hexadecimal =
+      content.length > 3 ? content.match(/[a-f\d]{2}/gi) : content.split('')
 
-  const color = hexadecimal
-    .map((value) => String(parseInt(value.length === 2 ? value : `${value}${value}`, 16)))
-    .join(', ')
+    const color = hexadecimal
+      .map((value) =>
+        String(parseInt(value.length === 2 ? value : `${value}${value}`, 16))
+      )
+      .join(', ')
 
-  return `rgba(${color}, ${alpha || 0})`
+    return `rgba(${color}, ${alpha || 0})`
+  } catch {
+    return hex
+  }
 }
