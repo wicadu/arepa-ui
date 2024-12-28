423f3211ac7c53010496594ba8b606db35d11693
Jonathan Araujo <jonathan@wicadu.com>
2024-12-28 04:20:52 +0000

    Added `groupItemsByPath` _utils_ for grouping items by a specified nested field value using a path

---
+++ src/utils/groupItemsByPath.ts
@@ -0,0 +1,29 @@
+import getObjectField from './getObjectField'
+
+type GroupRecord<T> = Record<string, T[]>
+
+/**
+ * Groups an array of items by a specified nested field (using a string path).
+ *
+ * @param items - The array of items to group.
+ * @param path - The path string representing the nested field to use for grouping.
+ * @returns A record where each key is a unique value of the specified field,
+ *          and the value is an array of items with that field value.
+ */
+function groupItemsByPath<T>(items: T[], path: string): GroupRecord<T> {
+  return items.reduce((groups: GroupRecord<T>, item: T) => {
+    const key = getObjectField(item, path)
+
+    if (key === undefined || key === null) return groups
+
+    if (!groups[key]) {
+      groups[key] = []
+    }
+
+    groups[key].push(item)
+
+    return groups
+  }, {} as GroupRecord<T>)
+}
+
+export default groupItemsByPath
